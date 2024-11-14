45f22881a604a25f6a89f9b5081baeab86743137
ebouilhol <emmanuel.bouilhol@gmal.com>
2024-11-14 16:01:30 +0100

    update widget

--- src/stainseparation/__init__.py
+++ src/stainseparation/__init__.py
@@ -1,21 +1,28 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
-from ._writer import write_multiple, write_single_image
+# Importer votre widget principal
+from ._widget import StainSeparationWidget
+
+# Si vous avez un reader, vous pouvez l'importer ici si nécessaire
+# from ._reader import napari_get_reader
+
+# Si vous avez des données d'exemple
+# from ._sample_data import make_sample_data
+
+# Si vous avez un writer
+# from ._writer import write_single_image, write_multiple
 
 __all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    # "napari_get_reader",
+    # "write_single_image",
+    # "write_multiple",
+    # "make_sample_data",
+    "StainSeparationWidget",
 )
+
+# Fournir le widget pour Napari
+def napari_experimental_provide_dock_widget():
+    return StainSeparationWidget
